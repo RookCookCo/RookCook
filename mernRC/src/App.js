31616b656e1b620584fcbc822a95a35ca81e4b3d
wangr17 <wangr17@rpi.edu>
2024-06-21 14:47:40 -0400

    "added background to local host and some more front end"

--- mernRC/src/App.js
+++ mernRC/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import background from './background.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const appStyle = {
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'contain', // or 'cover'
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: '100vh',
+        margin: 0
+    };
+
+    return (
+        <div className="App" style={appStyle}>
+            <header className="App-header">
+                <div className="top-bar">
+                    <div className="search-box">
+                        <label htmlFor="search">Quick recipe search: </label>
+                        <input type="text" id="search" name="search" />
+                    </div>
+                    <div className="auth-buttons">
+                        <button>Login</button>
+                        <button>Sign up</button>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
