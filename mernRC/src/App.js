b37a8f8e8f86ccbccae4f4c13efa470f79f5f661
Nicole Wu <143554135+Nicc041@users.noreply.github.com>
2024-06-24 10:47:38 -0400

    fixed basic homepage

--- mernRC/src/App.js
+++ mernRC/src/App.js
@@ -1,30 +1,29 @@
 // App.js
 import React from 'react';
 import background from './background.png';
+
 import './App.css';
 
 function App() {
     const appStyle = {
         backgroundImage: `url(${background})`,
-        backgroundSize: 'contain', // or 'cover'
-        backgroundRepeat: 'no-repeat',
+        backgroundSize: '100%', // or 'cover'
         backgroundPosition: 'center',
-        height: '100vh',
-        margin: 0
+        backgroundRepeat: 'no-repeat',
+        height: '97vh',
     };
 
     return (
         <div className="App" style={appStyle}>
             <header className="App-header">
-                <div className="top-bar">
-                    <div className="search-box">
-                        <label htmlFor="search">Quick recipe search: </label>
-                        <input type="text" id="search" name="search" />
-                    </div>
-                    <div className="auth-buttons">
-                        <button>Login</button>
-                        <button>Sign up</button>
-                    </div>
+                
+                <div className="search-box">
+                    <label htmlFor="search">Quick recipe search: </label>
+                    <input type="text" id="search" name="search" />
+                </div>
+                <div className="auth-buttons">
+                    <button>Login</button>
+                    <button>Sign up</button>
                 </div>
             </header>
         </div>
