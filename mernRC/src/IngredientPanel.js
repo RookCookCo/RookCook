37978136dfbbc3cc07a26f5b2c00cf38e18c1e35
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2024-07-17 23:08:39 -0400

    Fixed bug
    Fixed bug where selecting an ingredient from the list when there was only 1 ingredient displayed wasn't adding the ingredient to the inventory.

--- mernRC/src/IngredientPanel.js
+++ mernRC/src/IngredientPanel.js
@@ -1,25 +1,30 @@
 import React from 'react';
 
 const IngredientPanel = ({
-                             panelMode,
-                             setShowPanel,
-                             setPanelMode,
-                             ingredientSearchQuery,
-                             setIngredientSearchQuery,
-                             showIngredientList,
-                             setShowIngredientList,
-                             filteredIngredients,
-                             selectedIngredient,
-                             setSelectedIngredient,
-                             handleAddIngredient,
-                             inventory,
-                             handleDeleteIngredient
-                         }) => {
+    panelMode,
+    setShowPanel,
+    setPanelMode,
+    ingredientSearchQuery,
+    setIngredientSearchQuery,
+    showIngredientList,
+    setShowIngredientList,
+    filteredIngredients,
+    selectedIngredient,
+    setSelectedIngredient,
+    handleAddIngredient,
+    inventory,
+    handleDeleteIngredient
+}) => {
     return (
         <div className="ingredient-panel">
             <div className="panel-header">
                 <div>
-                    <button onClick={() => setShowPanel(false)}>Close</button>
+                    <button
+                        onClick={() => setShowPanel(false)}
+                        style={{ backgroundColor: 'red', color: 'white' }}
+                    >
+                        Close
+                    </button>
                 </div>
                 <div>
                     <button onClick={() => setPanelMode('add')}>Add</button>
@@ -39,26 +44,48 @@ const IngredientPanel = ({
                         style={{ width: '152px' }} // Adjust the width value as per your requirement
                     />
                     {showIngredientList && filteredIngredients.length > 0 && (
-                        <select
-                            size={Math.min(10, filteredIngredients.length)}
-                            value={selectedIngredient}
-                            onChange={(e) => {
-                                setSelectedIngredient(e.target.value);
-                                handleAddIngredient(e.target.value);
-                            }}
-                            onClick={(e) => {
-                                if (e.target.tagName === 'OPTION') {
-                                    handleAddIngredient(e.target.value);
-                                }
-                            }}
-                            style={{ height: 'auto', maxHeight: '200px', overflowY: 'auto' }} // Adjust the height dynamically
-                        >
-                            {filteredIngredients.map((ingredient, index) => (
-                                <option key={index} value={ingredient}>
-                                    {ingredient}
-                                </option>
-                            ))}
-                        </select>
+                        <div>
+                            {filteredIngredients.length > 1 ? (
+                                <select
+                                    size={Math.min(10, filteredIngredients.length)}
+                                    value={selectedIngredient}
+                                    onChange={(e) => {
+                                        setSelectedIngredient(e.target.value);
+                                        handleAddIngredient(e.target.value);
+                                    }}
+                                    onClick={(e) => {
+                                        if (e.target.tagName === 'OPTION') {
+                                            handleAddIngredient(e.target.value);
+                                        }
+                                    }}
+                                    style={{ height: 'auto', maxHeight: '200px', overflowY: 'auto' }} // Adjust the height dynamically
+                                >
+                                    {filteredIngredients.map((ingredient, index) => (
+                                        <option key={index} value={ingredient}>
+                                            {ingredient}
+                                        </option>
+                                    ))}
+                                </select>
+                            ) : (
+                                <div
+                                    onClick={() => {
+                                        const ingredient = filteredIngredients[0];
+                                        setSelectedIngredient(ingredient);
+                                        handleAddIngredient(ingredient);
+                                    }}
+                                    style={{
+                                        cursor: 'pointer',
+                                        backgroundColor: 'white',
+                                        padding: '5px',
+                                        border: '1px solid #ccc',
+                                        borderRadius: '4px',
+                                        marginTop: '5px'
+                                    }}
+                                >
+                                    {filteredIngredients[0]}
+                                </div>
+                            )}
+                        </div>
                     )}
                 </div>
             )}
