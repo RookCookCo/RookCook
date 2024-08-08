cfcdbe8be0116084816df79a206e846a3c0f3ffe
Rita Lei <leir@rpi.edu>
2024-08-08 15:43:36 -0400

    Revert "Testing DB for deploy"
    This reverts commit aac2d9c0d0cd87183696f3414dbfc35b7c224e9e.

--- mernRC/backend/connect.js
+++ mernRC/backend/connect.js
@@ -1,22 +1,25 @@
-// connect.js
-require('dotenv').config(); // Load environment variables from .env file
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Cluster12425:RookCook@cluster0.isn6pzj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const mongoose = require('mongoose');
-
-// MongoDB connection URI from environment variables
-const uri = process.env.MONGO_URI;
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
-    process.exit(1); // Exit process if connection fails
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-};
+});
 
-module.exports = connectDB;
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
