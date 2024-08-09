65328642f9f454563c3c48aaf921ca9b72509241
J1m4 <Jwang062504@gmail.com>
2024-08-09 15:15:39 -0400

    Update firebase.js
    commented firebase.js

--- mernRC/src/firebase.js
+++ mernRC/src/firebase.js
@@ -1,19 +1,26 @@
-// src/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+// Import necessary functions from the Firebase SDKs
+import { initializeApp } from 'firebase/app'; // Function to initialize the Firebase app
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'; // Firebase authentication functions
 
+// Firebase configuration object containing all necessary Firebase project credentials
 const firebaseConfig = {
-    apiKey: "AIzaSyAPWAgi2j0ptRS57UUPhaS54a0I9LA4ywg",
-    authDomain: "rookcook-be127.firebaseapp.com",
-    projectId: "rookcook-be127",
-    storageBucket: "rookcook-be127.appspot.com",
-    messagingSenderId: "118853939633",
-    appId: "1:118853939633:web:07e4ab36a1d6fcbe540d90",
-    measurementId: "G-QVFBBZRPS7"
+    apiKey: "AIzaSyAPWAgi2j0ptRS57UUPhaS54a0I9LA4ywg",           // Your Firebase API key
+    authDomain: "rookcook-be127.firebaseapp.com",              // Your Firebase Auth domain
+    projectId: "rookcook-be127",                               // Your Firebase project ID
+    storageBucket: "rookcook-be127.appspot.com",               // Your Firebase storage bucket
+    messagingSenderId: "118853939633",                         // Your Firebase messaging sender ID
+    appId: "1:118853939633:web:07e4ab36a1d6fcbe540d90",        // Your Firebase app ID
+    measurementId: "G-QVFBBZRPS7"                              // Your Firebase measurement ID
 };
 
+// Initialize Firebase app with the configuration object
 const app = initializeApp(firebaseConfig);
+
+// Get the Firebase authentication instance associated with the initialized app
 const auth = getAuth(app);
+
+// Create a new GoogleAuthProvider instance for Google sign-in
 const provider = new GoogleAuthProvider();
 
+// Export the auth object, GoogleAuthProvider instance, and Firebase authentication functions
 export { auth, provider, signInWithPopup, signOut };
